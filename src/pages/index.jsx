37c6e6f83c5aeee9c5e0c8b9a16f61ee13037f8f
medvedium <mumbles2606@gmail.com>
2025-02-13 09:35:37 +0300

    Main structure done

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+	title: 'Home',
+	description: 'Stream Vibe is a streaming platform for movies, TV shows, and more.',
+}
+export default function () {
+	return (
+		<>
+			<h1>Главная</h1>
+		</>
+	)
+}
---
+++ src/pages/movies.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+	title: 'Movies & Shows',
+	description: 'Stream Vibe is a streaming platform for movies, TV shows, and more.',
+}
+export default function () {
+	return (
+		<>
+			<h1>Фильмы</h1>
+		</>
+	)
+}
