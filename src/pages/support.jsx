56a8c20d35ffbe614cbca837129ef3ee26e087fe
Aleksey <mumbles2606@gmail.com>
2025-05-13 17:06:48 +0300

    Created Support page, added base structure, added Field component

---
+++ src/pages/support.jsx
@@ -0,0 +1,14 @@
+import Support from "@/sections/Support"
+
+export const metadata = {
+  title: "Support",
+  description:
+    "Stream Vibe is a streaming platform for movies, TV shows, and more.",
+}
+export default function () {
+  return (
+    <>
+      <Support />
+    </>
+  )
+}
