049f18e7efdda6d47f4c8ee44df61f82914b8fef
Aleksey <mumbles2606@gmail.com>
2025-05-13 10:14:56 +0300

    Show page - added intro block

---
+++ src/pages/show.jsx
@@ -0,0 +1,14 @@
+import ShowBanner from "@/sections/ShowBanner"
+
+export const metadata = {
+  title: "Show - Stranger Things",
+  description:
+    "Stream Vibe is a streaming platform for movies, TV shows, and more.",
+}
+export default function () {
+  return (
+    <>
+      <ShowBanner />
+    </>
+  )
+}
---
+++ src/sections/ShowBanner/ShowBanner.jsx
@@ -0,0 +1,22 @@
+import MovieBannerCard from "@/components/MovieBannerCard"
+
+const ShowBanner = () => {
+  const titleId = "show-banner-title"
+
+  return (
+    <seciton className={"container"} aria-labelledBy={titleId}>
+      <MovieBannerCard
+        title={"Stranger Things"}
+        titleId={titleId}
+        TitleTag={"h1"}
+        description={
+          "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and one strange little girl."
+        }
+        imgSrc={"/src/assets/images/movie-banner/3.jpg"}
+        isSmallPaddingY
+      />
+    </seciton>
+  )
+}
+
+export default ShowBanner
